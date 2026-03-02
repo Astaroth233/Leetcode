c9432dc0a31da3ac9481bdae2951c79191a522ac
Rohan G <113969234+Astaroth233@users.noreply.github.com>
2026-03-03 00:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 20.6 MB (82.78%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,32 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int c = 1;
-        sort(nums.begin(), nums.end());
-        int curr = nums[0];
-        for(int i=1;i<nums.size();i++)
+        int res = 0;
+        for(int n : nums)
         {
-            if(nums[i] == curr)
-            {
-                c ++;
-            }
-            else
-            {
-                if(c == 1)
-                {
-                    return curr;
-                }
-                else
-                {
-                    c = 1;
-                    curr = nums[i];
-                }
-            }
+            res ^= n;
         }
-        if(c == 1)
-        {
-            return curr;
-        }
-        return -1;
+        return res;
     }
 };
