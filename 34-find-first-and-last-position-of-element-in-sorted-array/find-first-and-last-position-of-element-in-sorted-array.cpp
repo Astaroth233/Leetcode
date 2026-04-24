0cd950afc317eeb5e9311c5f876f39b3488587d0
Rohan G <113969234+Astaroth233@users.noreply.github.com>
2026-04-24 16:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (18.91%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,38 +1,24 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        int low = 0, high = nums.size() - 1;
+        int firstOccur = binSearch(nums, target, true);
+        int lastOccur = binSearch(nums, target, false);
+
+        return {firstOccur, lastOccur};
+    }
+
+    int binSearch(vector<int> &nums, int target, bool Lbias)
+    {
+        int res = -1;
+        int low = 0;
+        int high = nums.size()-1;
+
         while(low <= high)
         {
             int mid = low + (high - low) / 2;
             if(nums[mid] == target)
             {
-                int first = checkpos(nums, target, low, high, true);
-                int last = checkpos(nums, target, low, high, false);
-                return {first, last};
-            }
-            else if(nums[mid] > target)
-            {
-                high = mid - 1;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-        }
-        return {-1, -1};
-    }
-
-    int checkpos(vector<int>& a, int target,int left, int right, bool leftBias)
-    {
-        int low = left, high = right;
-        int res = -1;
-        while(low <= high)
-        {
-            int mid = low + (high - low) / 2;
-            if(a[mid] == target)
-            {
-                if(leftBias)
+                if(Lbias)
                 {
                     res = mid;
                     high = mid - 1;
@@ -43,7 +29,7 @@ public:
                     low = mid + 1;
                 }
             }
-            else if(a[mid] > target)
+            else if(nums[mid] > target)
             {
                 high = mid - 1;
             }
