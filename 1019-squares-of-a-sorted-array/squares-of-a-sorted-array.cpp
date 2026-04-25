0dd262c85a20a146864a8f43744a3caee7fda324
Rohan G <113969234+Astaroth233@users.noreply.github.com>
2026-04-25 16:22:26 +0530

    Time: 4 ms (42.74%) | Memory: 31.9 MB (7.96%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -1,11 +1,45 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for(int i=0;i<nums.size();i++)
+        vector<int> pos;
+        vector<int> neg;
+        vector<int> res(nums.size(), 0);
+
+        for(int i : nums)
         {
-            nums[i] = pow(nums[i], 2);
+            if(i >= 0)
+                pos.push_back(i);
+            else
+                neg.push_back(i);
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+
+        // square negatives
+        for(int i = 0; i < neg.size(); i++)
+            neg[i] = neg[i] * neg[i];
+
+        // reverse negatives to make sorted
+        reverse(neg.begin(), neg.end());
+
+        // square positives
+        for(int i = 0; i < pos.size(); i++)
+            pos[i] = pos[i] * pos[i];
+
+        // merge
+        int i = 0, j = 0, idx = 0;
+        while(i < pos.size() && j < neg.size())
+        {
+            if(pos[i] <= neg[j])
+                res[idx++] = pos[i++];
+            else
+                res[idx++] = neg[j++];
+        }
+
+        while(i < pos.size())
+            res[idx++] = pos[i++];
+
+        while(j < neg.size())
+            res[idx++] = neg[j++];
+
+        return res;
     }
 };
