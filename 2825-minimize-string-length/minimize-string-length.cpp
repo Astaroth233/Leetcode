3cadbda742c4161adaba1a9524545afda41fef59
Rohan G <113969234+Astaroth233@users.noreply.github.com>
2026-04-28 19:43:55 +0530

    Time: 6 ms (79.84%) | Memory: 12.8 MB (75.90%) - LeetSync

--- 2825-minimize-string-length/minimize-string-length.cpp
+++ 2825-minimize-string-length/minimize-string-length.cpp
@@ -2,21 +2,13 @@ class Solution {
 public:
     int minimizedStringLength(string s) {
         vector<int> store(26, 0);
-
+        int count = 0;
         for(char ch : s)
         {
             if(store[ch-'a'] == 0)
             {
                 store[ch-'a'] = 1;
-            }
-        }
-
-        int count = 0;
-        for(int i : store)
-        {
-            if(i == 1)
-            {
-                count ++;
+                count++;
             }
         }
         return count;
